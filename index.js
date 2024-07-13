24e362d62ec0f5efc3364f3308ae97a7c112431e
shihab <shahriarshihab96@gmail.com>
2024-07-14 03:26:58 +0600

    mobile responsive done

---
+++ index.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar'); 
+    sidebar.style.display = 'flex'; 
+}
+
+function hideSidebar() {
+    const hideSidebar = document.querySelector('.sidebar'); 
+
+    hideSidebar.style.display = 'none';
+}
